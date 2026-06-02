da00e5e75bec37411fc2328d7bbd59259ca65261
ken-rmndbn <remandabankenneth29@gmail.com>
2026-06-02 23:42:21 +0800

    creating code for colors

---
+++ p1_test_fan/output_color.py
@@ -0,0 +1,9 @@
+class OutputColor:
+    CYAN = '\033[96m'
+    YELLOW = '\033[93m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    PURPLE = '\033[95m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
