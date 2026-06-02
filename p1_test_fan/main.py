a3b4a18bc9c235fb4d2556c76d11bef2cafdfc61
ken-rmndbn <remandabankenneth29@gmail.com>
2026-06-03 00:44:01 +0800

    creating code to diplay the result

--- p1_test_fan/main.py
+++ p1_test_fan/main.py
@@ -13,3 +13,14 @@ def main():
     fan_2.set_radius(5)
     fan_2.set_color("blue")
     fan_2.set_on(False)
+
+    print(f"\n{OutputColor.BOLD}{OutputColor.PURPLE}   FAN TESTING REPORT  {OutputColor.END}\n")
+
+    print(f"{OutputColor.UNDERLINE}OBJECT 1 (Active Units){OutputColor.END}")
+    print(fan_1)
+
+    print(f"{OutputColor.UNDERLINE}OBJECT 2 (Storage Units){OutputColor.END}")
+    print(fan_2)
+
+if __name__ == "__main__":
+    main()
