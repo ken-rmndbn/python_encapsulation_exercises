b39f58ba21d6cb8e01e915a2505184c13dbd455c
ken-rmndbn <remandabankenneth29@gmail.com>
2026-06-02 23:51:55 +0800

    creating private data fields or for encapsulation

--- p1_test_fan/fan_modification.py
+++ p1_test_fan/fan_modification.py
@@ -5,4 +5,9 @@ class FanModification:
     MEDIUM = 2
     FAST = 3
 
-    
+    def __init__(self, speed = SLOW, radius = 5.0, color = "blue", on = False):
+        self.__speed = speed
+        self.__radius = float(radius)
+        self.__color = str(color)
+        self.__on = bool(on)
+        
