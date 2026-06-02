b7e2a2034445bf6298aa2e1a6ca5030968001693
ken-rmndbn <remandabankenneth29@gmail.com>
2026-06-03 00:30:09 +0800

    creating second fan object

--- p1_test_fan/main.py
+++ p1_test_fan/main.py
@@ -7,4 +7,9 @@ def main():
     fan_1.set_radius(10)
     fan_1.set_color("yellow")
     fan_1.set_on(True)
-    
+
+    fan_2 = FanModification()
+    fan_2.set_speed(FanModification.MEDIUM)
+    fan_2.set_radius(5)
+    fan_2.set_color("blue")
+    fan_2.set_on(False)
