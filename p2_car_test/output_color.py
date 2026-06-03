d6bd14ce16f30369a5817234cca05f8a53d44aee
ken-rmndbn <remandabankenneth29@gmail.com>
2026-06-04 00:49:26 +0800

    adjusting indentation

--- p2_car_test/output_color.py
+++ p2_car_test/output_color.py
@@ -1,6 +1,6 @@
 class OutputColor:
-GREEN = '\033[92m'
-RED = '\033[91m'
-CYAN = '\033[96m'
-BOLD = '\033[1m'
-RESET = '\033[0m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    CYAN = '\033[96m'
+    BOLD = '\033[1m'
+    RESET = '\033[0m'
