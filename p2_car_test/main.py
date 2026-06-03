340410ecf3ead65c2623441df211172c691bcf31
ken-rmndbn <remandabankenneth29@gmail.com>
2026-06-04 01:16:34 +0800

    creating code for 5 times acceleration

--- p2_car_test/main.py
+++ p2_car_test/main.py
@@ -5,3 +5,10 @@ def main():
     my_car = CarModification("2017", "Honda Fd")
     print(f"{OutputColor.BOLD}Testing Car: {my_car.get_info()}{OutputColor.RESET}")
     print("-" * 30)
+
+    print(f"\n{OutputColor.GREEN}Accelerating...{OutputColor.RESET}")
+    for car in range(5):
+        my_car.accelerate()
+        print(f"Current speed: {OutputColor.CYAN}{my_car.get_speed()}{OutputColor.RESET}")
+
+    
