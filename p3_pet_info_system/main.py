04ea7485f7e05bf673fe3cb38d33d3740aef9ea4
ken-rmndbn <remandabankenneth29@gmail.com>
2026-06-04 19:01:00 +0800

    creating an empty pet object

--- p3_pet_info_system/main.py
+++ p3_pet_info_system/main.py
@@ -1,2 +1,9 @@
 from pet_modification import PetModification
 from output_color import OutputColor as Color
+
+def main():
+    print(f"{Color.HEADER}{Color.BOLD}    PET INFORMATION SYSTEM    {Color.ENDC}\n")
+
+    my_pet = PetModification()
+
+    
