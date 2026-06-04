a413a033353a3645b37f4a8e9efd2789b5b907ca
ken-rmndbn <remandabankenneth29@gmail.com>
2026-06-04 18:51:46 +0800

    creating mutators or setters

--- p3_pet_info_system/pet_modification.py
+++ p3_pet_info_system/pet_modification.py
@@ -4,4 +4,12 @@ class PetModification:
         self.__animal_type = animal_type
         self.__age = age
 
+    def __set_name__(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
         
