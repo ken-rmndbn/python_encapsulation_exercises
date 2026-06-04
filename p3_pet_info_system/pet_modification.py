bfd840021e64ca9a1348deec309761305c4dc34b
ken-rmndbn <remandabankenneth29@gmail.com>
2026-06-04 18:48:43 +0800

    creating attributes or the encapsulation

--- p3_pet_info_system/pet_modification.py
+++ p3_pet_info_system/pet_modification.py
@@ -1,3 +1,7 @@
 class PetModification:
     def __init__(self, name = "", animal_type = "", age = 0):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+
         
